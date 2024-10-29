4bfe399c28dced9cd24385e59e59d93aeed68150
RiyaIdikkula <78250562+RiyaIdikkula@users.noreply.github.com>
2024-10-29 16:00:07 +0530

    Update settings.py

--- ecom_project/settings.py
+++ ecom_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5j^=k5xg*g@jt-n*c0*oigvz#mk4x@rmv%z+ze4%($(b2u(1e%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['standard-salli-weddingplanner-83952bdd.koyeb.app']
 
 
 # Application definition
